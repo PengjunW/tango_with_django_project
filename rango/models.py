b5eb5c7e00002654423dc82e98893dbbd1b0f28d
wpj <wpj319@gmail.com>
2021-07-29 21:47:56 +0800

    chapter5 completed successfully

--- rango/models.py
+++ rango/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
